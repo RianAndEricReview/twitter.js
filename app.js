892b8bfb16e1504f57287e043771bf567ec03fde
Eric Garnett <egcodes@gmail.com>
2018-03-01 10:57:16 -0500

    set up nunjucks for rendering html

--- app.js
+++ app.js
@@ -3,11 +3,23 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+
+let title = 'An example';
+let people = [{name: 'Gandalf'}, {name: 'Frodo'}, {name: 'Hermione'}];
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true});
+// nunjucks.render('index.html', {title: title, people: people}, (err, output) => {
+//   if (err) throw err;
+//   console.log(output);
+// });
 
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.render('index', {title: title, people: people});
 });
 
 app.get('/news', (req, res) => {
