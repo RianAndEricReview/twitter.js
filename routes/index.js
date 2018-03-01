31b1fa49c40a83bf1e1f8a6ab7ce28d904c58867
Rian Halperin <rianhalperin@gmail.com>
2018-03-01 14:40:57 -0500

    twitter.js, Add form field

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,19 @@ const tweetBank = require('../tweetBank');
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+  res.render( 'index', { tweets: tweets, showForm: true } );
 });
 
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
   var list = tweetBank.find( {name: name} );
-  res.render( 'index', { tweets: list } );
+  res.render( 'index', { tweets: list} );
 });
 
 router.get('/tweets/:id', function(req, res) {
   var id = +req.params.id;
   var tweet = tweetBank.find( {id: id} );
-  res.render( 'index', { tweets: tweet } );
+  res.render( 'index', { tweets: tweet, showForm: true } );
 });
 
 module.exports = router;
